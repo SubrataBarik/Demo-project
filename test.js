f33516012d54eba605822cca313b94a90fa2a131
SubratBarik <bariksubrat021@gmail.com>
2024-11-17 10:40:27 +0530

    new one

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
 console.log("subrat")
+
+let n=[]
+let s=[]
+let b=[]
+let a=['subrat' ,123,44 ,true ,'papulu']
+function f(a){
+    for(i of a){
+        t=typeof(i)
+        if(t=='number')
+            n.push(i)
+        if(t=='string')
+            s.push(i)
+        if(t=='boolean')
+            b.push(i)
+    }
+}
+f(a)
+console.log(n,s,b);
